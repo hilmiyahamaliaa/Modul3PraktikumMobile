3db83bc748610ea61688442d644ca4212b91f4f9
hilmiyahamaliaa <hilmiyahamaliaa@gmail.com>
2022-04-14 23:28:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/modul3/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.modul3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.modul3.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.konversibutton.setOnClickListener{
+            konversiTip() }
+    }
+    
+    fun konversiTip() {
+    val stringInTextField = binding.nilai.text.toString()
+    val hitung = stringInTextField.toDouble()
+    val selectedId = binding.radioGroup.checkedRadioButtonId
+    val konversi = when (selectedId) {
+        R.id.Euro -> 15620.81
+        R.id.Usd -> 14366.00
+        R.id.Yen -> 114.51
+        else -> 03831.34
+    }
+        var result = konversi * hitung
+
+        val formattedconvert = NumberFormat.getCurrencyInstance().format(result)
+        binding.result.text = getString(R.string.konversi, formattedconvert)
+    }
+}
---
+++ app/src/test/java/com/example/modul3/ActivityMainBinding.kt
@@ -0,0 +1,5 @@
+package com.example.modul3
+
+class ActivityMainBinding {
+
+}
